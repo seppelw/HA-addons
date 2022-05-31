8070895b0090a5e94788561063bc1211f739cd4e
Seppel <wiechers.sebastian@gmail.com>
2022-05-31 15:44:34 +0200

    test

--- tuya-cli/rootfs/etc/cont-init.d/20-dirs.sh
+++ tuya-cli/rootfs/etc/cont-init.d/20-dirs.sh
@@ -3,8 +3,8 @@
 # Home Assistant Community Add-on: Tor
 # Ensures necessary directories exists
 # ==============================================================================
-if ! bashio::fs.directory_exists '/ssl/tor'; then
-    mkdir -p /ssl/tor/hidden_service \
-        || bashio::exit.nok 'Could not create tor data directory'
-    chmod -R 0700 /ssl/tor
-fi
+#if ! bashio::fs.directory_exists '/ssl/tor'; then
+ #   mkdir -p /ssl/tor/hidden_service \
+  #      || bashio::exit.nok 'Could not create tor data directory'
+   # chmod -R 0700 /ssl/tor
+#fi
